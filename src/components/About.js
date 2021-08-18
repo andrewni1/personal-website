5ca5fe16bf772de0cd8b246e9628e520f1a3d25b
unknown <ni95@purdue.edu>
2021-08-18 12:37:55 -0400

    Fully responsive

--- src/components/About.js
+++ src/components/About.js
@@ -7,13 +7,15 @@ function About() {
     return (
         <div className="about-container">
             <h1 className="header">About Me</h1>
-            <div className="contents">
-                <p className="desc">Hello, my name is Andrew! I am a sophomore attending Purdue University, seeking a Bachelor's Degree in Computer Science. 
-                    My area of interest in the CS field lies in full stack development. I am always looking to create products with a seemless interface on
-                    the frontend while providing product functionality on the backend. 
-                    <br /> <br />
-                    Some of my many interests include the stock market, blackjack, weight lifting, and poker.
-                </p>
+            <div className="contents-container">
+                <div className="contents">
+                    <p className="desc">Hello, my name is Andrew! I am a sophomore attending Purdue University, seeking a Bachelor's Degree in Computer Science. 
+                        My area of interest in the CS field lies in full stack development. I am always looking to create products with a seemless interface on
+                        the frontend while providing product functionality on the backend. 
+                        <br /> <br />
+                        Some of my many interests include the stock market, blackjack, weight lifting, and poker.
+                    </p>
+                </div>
                 <img className="picture" src={MyPic} alt="pic"/>
             </div>
         </div>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import Github from '../pictures/github.png'
 import Python from '../pictures/python.png'
+import ProjPic from '../pictures/projPic.png'
+import ExternalLink from '../pictures/externalLink.png'
 import './projects.css'
 
 function Projects() {
@@ -8,11 +10,11 @@ function Projects() {
         <div>
             <h1 className="header">Projects</h1>
             <div className="content-container">
-                <img className="experience-icon" src={Python} alt="python"/>
-                <div className="experience-header">
-                    <h2 className="experience-org">Product Monitor</h2>
-                    <h3 className="experience-timeframe">https://github.com/andrewni1/bestbuy-monitor</h3>
-                    <p className="experience-description">
+                <img className="projects-icon" src={Python} alt="python"/>
+                <div className="projects-header">
+                    <h2 className="projects-org">Product Monitor</h2>
+                    <h3 className="projects-link">https://github.com/andrewni1/product-monitor</h3>
+                    <p className="projects-description">
                         - Developed an application with Python that monitors whether certain products are in stock or are restocking
                         on by scraping product details
                         <br />
@@ -20,8 +22,24 @@ function Projects() {
                         <br />
                         - Monitor alerts display product name, link, SKU, price, and an add to cart button which allow users to add the product straight to cart through the use of BestBuy's API
                         <br />
-                        <p className="experience-technologies">Python | Discord.py</p>
-                        <a href="https://github.com/andrewni1/bestbuy-monitor" target="_blank" rel="noreferrer"><img className="experience-github" src={Github} alt="github"/></a>
+                        <p className="projects-technologies">Python | Discord.py</p>
+                        <a href="https://github.com/andrewni1/product-monitor" target="_blank" rel="noreferrer"><img className="experience-github" src={Github} alt="github"/></a>
+                    </p>
+                </div>
+            </div>
+            <div className="content-container">
+                <img className="projects-icon" src={ProjPic} alt="proj-pic"/>
+                <div className="projects-header">
+                    <h2 className="projects-org">Personal Website</h2>
+                    <h3 className="projects-link">andrewni.com</h3>
+                    <p className="projects-description">
+                        - Developed a personal website by using the React.js framework
+                        <br />
+                        - Showcases my experience and the projects that I have worked on
+                        <br />
+                        <p className="projects-technologies">JavaScript | React.js</p>
+                        <a href="https://github.com/andrewni1/product-monitor" target="_blank" rel="noreferrer"><img className="experience-github" src={Github} alt="github"/></a>
+                        <a href="https://andrewni.com/" target="_blank" rel="noreferrer"><img className="experience-github" src={ExternalLink} alt="external-link"/></a>
                     </p>
                 </div>
             </div>
