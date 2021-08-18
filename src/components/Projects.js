54bc252ee4d8374ff6d2f7ce9c7e15e1572a55d0
unknown <ni95@purdue.edu>
2021-08-18 13:38:07 -0400

    Link and tab changes

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -31,14 +31,14 @@ function Projects() {
                 <img className="projects-icon" src={ProjPic} alt="proj-pic"/>
                 <div className="projects-header">
                     <h2 className="projects-org">Personal Website</h2>
-                    <h3 className="projects-link">andrewni.com</h3>
+                    <h3 className="projects-link">www.andrewni.com</h3>
                     <p className="projects-description">
                         - Developed a personal website by using the React.js framework
                         <br />
                         - Showcases my experience and the projects that I have worked on
                         <br />
                         <p className="projects-technologies">JavaScript | React.js</p>
-                        <a href="https://github.com/andrewni1/product-monitor" target="_blank" rel="noreferrer"><img className="experience-github" src={Github} alt="github"/></a>
+                        <a href="https://github.com/andrewni1/personal-website" target="_blank" rel="noreferrer"><img className="experience-github" src={Github} alt="github"/></a>
                         <a href="https://andrewni.com/" target="_blank" rel="noreferrer"><img className="experience-github" src={ExternalLink} alt="external-link"/></a>
                     </p>
                 </div>
