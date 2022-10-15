4408e410e000f3d591981d770762dff088a5b5ba
unknown <ni95@purdue.edu>
2022-10-15 09:51:30 -0400

    About section minor change

--- src/components/About.js
+++ src/components/About.js
@@ -8,7 +8,7 @@ function About() {
             <h1 className="header">About Me</h1>
             <div className="contents-container">
                 <div className="contents">
-                    <p className="desc">Hello, my name is <b>Andrew Ni</b>! I am a rising junior attending Purdue University, seeking a <b>Bachelor's Degree</b> in <b>Computer Science</b>. 
+                    <p className="desc">Hello, my name is <b>Andrew Ni</b>! I am a junior attending Purdue University, seeking a <b>Bachelor's Degree</b> in <b>Computer Science</b>. 
                         My area of interest in the CS field lies in <b>full stack development</b>. I'm always looking to develop products with an appealing user interface on
                         the <b>frontend</b> while implementing seemless functionality on the <b>backend</b>. 
                         <br /> <br />
